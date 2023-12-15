73e955757ef31b8ac06f9824018cf9d8d88f2286
Th1Exterminator <atillmand@gmail.com>
2023-12-15 01:48:16 -0600

    first commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#define GLFW_INCLUDE_VULKAN
+#include "GLFW/glfw3.h"
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+int main()
+{
+    std::cout << "Hello world" << std::endl;
+
+    return 0;
+}
